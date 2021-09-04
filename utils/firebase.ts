7b021474f90745368dc6ff9ac70954416b1ce4b6
Yog <yog.panjarale@gmail.com>
2021-09-04 19:07:58 +0530

    firebase tailwind added

---
+++ utils/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBMmGNtsBfJ9jNWvzC6Jdg1aKajTx0dNSk",
+  authDomain: "null-pointers-roadmappr.firebaseapp.com",
+  projectId: "null-pointers-roadmappr",
+  storageBucket: "null-pointers-roadmappr.appspot.com",
+  messagingSenderId: "966349030038",
+  appId: "1:966349030038:web:078517e2a0d70d31c778b7",
+  measurementId: "G-4MKJYJ1YZH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
